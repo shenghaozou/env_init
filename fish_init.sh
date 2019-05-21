82622da6afe0a77f74955763cb390da972f3e771
Frank Zou <szou28@wisc.edu>
2019-05-21 10:40:25 -0500

    update scripts

--- fish_init.sh
+++ fish_init.sh
@@ -1,9 +1,13 @@
-echo "alias rgv \"docker run --runtime=runsc -it --name=gvisor test_image /bin/bash\"" >> ~/.config/fish/config.fish
-echo "Added alias rgv (run gvisor container) to fish rc"
+echo "alias rgc \"docker run --runtime=runsc -it --name=gvisor test_image /bin/bash\"" >> ~/.config/fish/config.fish
+echo "Added alias rgc (run gvisor container) to fish rc"
 
 echo "alias dsp \"docker system prune\"" >> ~/.config/fish/config.fish
 echo "Added alias dsp (docker system prune) to fish rc"
 
+echo "alias rgc2 \"dsp;rgc\"" >> ~/.config/fish/config.fish
+
 echo "alias dbt \"docker build -t test_image ~/image\"" >> ~/.config/fish/config.fish
 echo "Added alias dbt (docker build test_image) to fish rc"
 
+echo "alias cprunsc \"sudo cp ./bazel-bin/runsc/linux_amd64_pure_stripped/runsc /usr/local/bin\"" >> ~/.config/fish/config.fish
+echo "Added alias cprunsc to fish rc"
